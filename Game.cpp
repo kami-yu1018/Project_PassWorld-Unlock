ab0f7798a633705f688fe4e64381e1c25c6b22c4
4A-28\koyo <a>
2026-07-06 12:10:37 +0900

    各インスタンスの更新処理関数と描画関数をGameクラスの更新処理関数、描画処理関数に追加

--- Game.cpp
+++ Game.cpp
@@ -16,18 +16,23 @@ void Game::Update()
 	switch (State)
 	{
 	case GAME_STATE_TITLE:		//　タイトル画面
+		title_obj.Update();
 		break;
 
-	case GAME_STATE_LEVEL:		//　レベル選択画面	
+	case GAME_STATE_LEVEL:	//　レベル選択画面	
+		level_obj.Update();
 		break;
 
 	case GAME_STATE_SETTING:	//　パーティー編成画面
+		set_obj.Update();
 		break;
 
 	case GAME_STATE_PLAY:		//	バトル画面
+		play_obj.Update();
 		break;
 
 	case GAME_STATE_END:		//	終了画面
+		end_obj.Update();
 		break;
 	}
 }
@@ -39,18 +44,23 @@ void Game::Render()
 	switch (State)
 	{
 	case GAME_STATE_TITLE:		//　タイトル画面
+		title_obj.Render();
 		break;
 
 	case GAME_STATE_LEVEL:		//　レベル選択画面	
+		level_obj.Render();
 		break;
 
 	case GAME_STATE_SETTING:	//　パーティー編成画面
+		set_obj.Render();
 		break;
 
 	case GAME_STATE_PLAY:		//	バトル画面
+		play_obj.Render();
 		break;
 
 	case GAME_STATE_END:		//	終了画面
+		end_obj.Render();
 		break;
 	}
 }
