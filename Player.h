daf4fe4a4dba9c085b467e576475022165bfc946
4A-28\koyo <a>
2026-07-06 10:02:34 +0900

    Playerクラスの設定 PlayerData構造体とコンストラクタをクラス内で定義

--- Player.h
+++ Player.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "Data.h"
+
+// @brief プレイヤークラス
+// プレイヤーのステータスや行動を管理するクラス
+class Player
+{
+public:
+	// PlayerData構造体
+	PlayerData Data;
+
+	// コンストラクタ
+	Player(const PlayerData& data) :Data(data)
+	{
+	}
+};
