2e65894bfb8fd30c030fd2cd312de91f58521109
4A-28\koyo <a>
2026-07-06 10:07:41 +0900

    Enemyクラスの設定 クラス内でEnemyData構造体の宣言、コンストラクタの定義

--- Enemy.h
+++ Enemy.h
@@ -1,1 +1,15 @@
 #pragma once
+#include"Data.h"
+
+// @brief 敵クラス
+// 敵のステータスや行動を管理するクラス
+class Enemy
+{
+public:
+	// EnemyData構造体
+	EnemyData Data;
+	// コンストラクタ
+	Enemy(const EnemyData&data):Data(data)
+	{ 
+	}
+};
