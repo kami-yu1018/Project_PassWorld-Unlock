9ba4bd59645a277b62d78ad97e28aaa842e92da9
4A-28\koyo <a>
2026-07-06 09:58:02 +0900

    プレイヤーデータの定義 プレイヤーを生成するCreatePlayer関数の定義 Playerが不完全な型であるためPlayerクラスを定義する必要がある

---
+++ PlayerFactory.cpp
@@ -0,0 +1,28 @@
+#include"Factory.h"
+#include"Data.h"
+
+//　プレイヤーデータは不変（基礎ステータスは不変でレベルによって数値変動する）なので静的で定義する
+const PlayerData PlayerFactory::PlayerTable[] = {
+	{1,"剣士",100,80,100,100,75},
+	{2,"魔法使い",80,120,100,80,60},
+	{3,"僧侶",80,120,50,80,50},
+	{4,"盗賊",70,50,75,60,100},
+	{5,"重騎士",135,50,80,150,30},
+	{6,"武士",75,50,100,75,70},
+	{7,"吟遊詩人",75,80,50,75,80}
+};
+
+//　変数、関数がメモリ上で占有するサイズで割ってデータテーブルのサイズを求める
+const int PlayerFactory::PlayerTableSize = sizeof(PlayerTable) / sizeof(PlayerData);
+
+Player* PlayerFactory::CreatePlayer(int ID)
+{
+	for (int i = 0; i < PlayerTableSize; ++i)
+	{
+		if (PlayerTable[i].ID == ID)
+		{
+			return new Player(PlayerTable[i]);
+		}
+	}
+	return nullptr;
+}
