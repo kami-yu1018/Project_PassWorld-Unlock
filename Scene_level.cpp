84a26af1ce51dd8431325ca443b4de81b6532bc4
4A-28\koyo <a>
2026-07-13 10:06:02 +0900

    Scene_play.cpp以外のシーンに実装するべきものをコメントで指定

--- Scene_level.cpp
+++ Scene_level.cpp
@@ -7,12 +7,20 @@ void Scene_level::Init()
 		   
 void Scene_level::Update()
 {		   
-		   
+	//	上下キーで難易度の変更
+
+	//	選択している難易度でshiftキーを押すと、敵のビジュアルと説明を表示
+
+	//	エンターを押したら難易度を確定
+	//	（Scene_playで難易度を渡せるようにする）
+	  
 }		   
 		   
 void Scene_level::Render()
 {		   
-		   
+	//	背景
+
+	//	難易度の画像
 }		   
 		   
 void Scene_level::Exit()
--- Scene_title.cpp
+++ Scene_title.cpp
@@ -7,11 +7,18 @@ void Scene_title::Init()
 
 void Scene_title::Update()
 {
+	//	キーを押したら次のシーンへ移動
 
 }
 
 void Scene_title::Render()
 {
+	//	背景
+
+	//	タイトル
+
+	//	"Push -Key To Start"のテキスト
+
 
 }
 
