6fa0306d683f75216b742b8761c98a7f1696e7a7
4A-28\koyo <a>
2026-08-31 11:45:35 +0900

    各キャラクターのスキルを設定

--- Data.h
+++ Data.h
@@ -1,6 +1,27 @@
 #pragma once
 #include<string>
 
+//	スキル数
+constexpr int SKILL_COUNT = 3;
+
+enum
+{
+	TYPE_ATTACK,
+	TYPE_DEF,
+	TYPE_HEAL,
+	TYPE_BUF,
+};
+
+//	スキルデータ
+struct SkillData
+{
+	int ID;
+	const char* name;		//	スキル名
+	const char* password;	//	パスワード
+	float Rate;				//	倍率
+	int ratetype;			//	効果タイプ
+};
+
 // プレイヤーのデータ構造体
 struct PlayerData
 {
@@ -17,6 +38,8 @@ struct PlayerData
 	int g_atk;
 	int g_def;
 	int g_spd;
+
+	SkillData skills[SKILL_COUNT];
 };
 
 //	敵のデータ構造体
